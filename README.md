dcad32cceb48dfca5dda49c0dfc01005ed152014
shiyu1314 <shiyu1052@gmail.com>
2026-08-05 14:04:59 +0800

    25.12

---
+++ README.md
@@ -0,0 +1,24 @@
+# RAX3000M OpenWrt 闭源驱动固件
+
+## 📋 支持的设备
+- ✅ **RAX3000M EMMC 版**
+- ✅ **RAX3000M NAND 版**
+- ✅ **RAX3000Me**
+
+## ⚡ 核心特性
+- **自建源支持**
+- **集成nginx**
+- **中继增强**
+- **最新25.12**：6.12内核
+- **闭源驱动支持**：**wed hnat MTK 硬件加速**
+- **支持bin与itb固件**
+
+
+## 🔧 U-boot
+- [bl-mt798x-dhcpd](https://github.com/Yuzhii0718/bl-mt798x-dhcpd)
+
+## chasey-dev
+- [核心源码](https://github.com/chasey-dev/immortalwrt-mt798x-rebase)
+
+## sbwml
+- [部分补丁来自](https://github.com/sbwml/r4s_build_script)
---
+++ sh/op.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+set -x
+
+# kenrel Vermagic
+sed -ie 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+grep HASH target/linux/generic/kernel-6.12 | awk -F'HASH-' '{print $2}' | awk '{print $1}' | md5sum | awk '{print $1}' > .vermagic
+
+git clone -b packages --depth 1 --single-branch https://github.com/shiyu1314/openwrt-feeds package/xd
+git clone -b porxy --depth 1 --single-branch https://github.com/shiyu1314/openwrt-feeds package/porxy
+
+rm -rf feeds/luci/applications/{luci-app-dockerman,luci-app-samba4,luci-app-aria2,luci-app-diskman}
+rm -rf feeds/packages/net/{samba4,v2ray-geodata,mosdns,sing-box,aria2,ariang,adguardhome}
+
+# drop attendedsysupgrade
+sed -i '/luci-app-attendedsysupgrade/d' \
+    feeds/luci/collections/luci-nginx/Makefile \
+    feeds/luci/collections/luci-ssl-openssl/Makefile \
+    feeds/luci/collections/luci-ssl/Makefile \
+    feeds/luci/collections/luci/Makefile
+    
+sed -i 's/+uhttpd /+luci-nginx /g' feeds/luci/collections/luci/Makefile
+sed -i 's/+uhttpd-mod-ubus //' feeds/luci/collections/luci/Makefile
+sed -i 's/+uhttpd /+luci-nginx /g' feeds/luci/collections/luci-light/Makefile
+sed -i "s/+luci /+luci-nginx /g" feeds/luci/collections/luci-ssl-openssl/Makefile
+sed -i "s/+luci /+luci-nginx /g" feeds/luci/collections/luci-ssl/Makefile
+sed -i 's/+uhttpd +uhttpd-mod-ubus /+luci-nginx /g' feeds/packages/net/wg-installer/Makefile
+sed -i '/uhttpd-mod-ubus/d' feeds/luci/collections/luci-light/Makefile
+sed -i 's/+luci-nginx \\$/+luci-nginx/' feeds/luci/collections/luci-light/Makefile
+
+pushd feeds/luci || exit 1
+for patch in *.patch; do
+    [ -f "$patch" ] || continue
+    
+    echo "Applying $patch ..."
+    patch -p1 --no-backup-if-mismatch < "$patch" || {
+        echo "ERROR: Failed to apply $patch"
+        popd
+        exit 1
+    }
+done
+popd
+
+for patch in *.patch; do
+    [ -f "$patch" ] || continue
+    
+    echo "Applying $patch ..."
+    patch -p1 --no-backup-if-mismatch < "$patch" || {
+        echo "ERROR: Failed to apply $patch"
+        popd
+        exit 1
+    }
+done
+
+
+# rust
+RUST_VERSION=1.95.0
+RUST_HASH=62b67230754da642a264ca0cb9fc08820c54e2ed7b3baba0289876d4cdb48c08
+sed -ri "s/(PKG_VERSION:=)[^\"]*/\1$RUST_VERSION/;s/(PKG_HASH:=)[^\"]*/\1$RUST_HASH/" feeds/packages/lang/rust/Makefile
+
+# fstools
+rm -rf package/system/fstools
+git clone https://github.com/sbwml/package_system_fstools -b openwrt-25.12 package/system/fstools
+# util-linux
+rm -rf package/utils/util-linux
+git clone https://github.com/sbwml/package_utils_util-linux -b openwrt-25.12 package/utils/util-linux
+
+# nghttp3
+rm -rf feeds/packages/libs/nghttp3
+git clone https://github.com/sbwml/package_libs_nghttp3 package/libs/nghttp3
+
+# ngtcp2
+rm -rf feeds/packages/libs/ngtcp2
+git clone https://github.com/sbwml/package_libs_ngtcp2 package/libs/ngtcp2
+
+# curl - fix passwall `time_pretransfer` check
+rm -rf feeds/packages/net/curl
+git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
+
+# nginx - latest version
+rm -rf feeds/packages/net/nginx
+git clone https://github.com/sbwml/feeds_packages_net_nginx feeds/packages/net/nginx -b openwrt-25.12
+sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g;s/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/net/nginx/files/nginx.init
+
+# nginx - ubus
+sed -i 's/ubus_parallel_req 2/ubus_parallel_req 6/g' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+sed -i '/ubus_parallel_req/a\        ubus_script_timeout 300;' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+
+# nginx-util
+sed -i '/\/etc\/nginx\/uci.conf.template/d' feeds/packages/net/nginx-util/Makefile
+
+# uwsgi - fix timeout
+sed -i '$a cgi-timeout = 600' feeds/packages/net/uwsgi/files-luci-support/luci-*.ini
+sed -i '/limit-as/c\limit-as = 5000' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+# disable error log
+sed -i "s/procd_set_param stderr 1/procd_set_param stderr 0/g" feeds/packages/net/uwsgi/files/uwsgi.init
+
+# uwsgi - performance
+sed -i 's/threads = 1/threads = 2/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+sed -i 's/processes = 3/processes = 4/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+sed -i 's/cheaper = 1/cheaper = 2/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+
+# rpcd - fix timeout
+sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
+sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
+
+# luci-compat - remove extra line breaks from description
+sed -i '/<br \/>/d' feeds/luci/modules/luci-compat/luasrc/view/cbi/full_valuefooter.htm
+
+
+#golang 26.x
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+
+sudo rm -rf package/base-files/files/etc/banner
+
+sed -i "s/%D %V %C/%D %V $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
+
+sed -i "s/%R/by $OP_author/" package/base-files/files/etc/openwrt_release
+
+date=$(date +"%Y-%m-%d")
+
+
+echo "                                                    " >> package/base-files/files/etc/banner
+echo "  _______                     ________        __" >> package/base-files/files/etc/banner
+echo " |       |.-----.-----.-----.|  |  |  |.----.|  |_" >> package/base-files/files/etc/banner
+echo " |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|" >> package/base-files/files/etc/banner
+echo " |_______||   __|_____|__|__||________||__|  |____|" >> package/base-files/files/etc/banner
+echo "          |__|" >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
+echo "         %D ${date} by $OP_author                     " >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
